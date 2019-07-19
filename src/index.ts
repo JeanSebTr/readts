c3b32d9d91fb103e9c218f88fd34c40e2eaf6c55
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2019-07-19 15:34:00 -0400

    Add typescript version check

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Copyright (c) 2016- readts authors, see AUTHORS.
 // Released under the MIT license, see LICENSE.
+import * as ts from 'typescript';
 
 export { IdentifierSpec } from './IdentifierSpec';
 export { SignatureSpec } from './SignatureSpec';
@@ -10,3 +11,9 @@ export { ModuleSpec } from './ModuleSpec';
 export { IndexSpec } from './IndexSpec';
 export { TypeSpec, FormatHook, FormatHooks } from './TypeSpec';
 export { Parser, SourcePos, RefSpec } from './Parser';
+export function assertTsVersion(version: string): boolean {
+    if(ts.version != version) {
+        throw new Error('Typescript version mismatch with readts');
+    }
+    return true;
+}
